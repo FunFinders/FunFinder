0539e9d18c8046a598e66807a62b2175915ec38a
Kevin Mach <kevinmach.cs@gmail.com>
2026-02-13 18:14:22 -0800

    Add user model class

--- src/data.py
+++ src/data.py
@@ -9,3 +9,10 @@ class Price(Enum):
     PRICE_VERY_EXPENSIVE = 4
 
 Coordinates = namedtuple("Coordinates", "longitude latitude")
+
+class UserModel:
+    def __init__(self):
+        self.id = -1
+        self.saved = []
+        self.preferred_types = []
+        self.liked_places = []
