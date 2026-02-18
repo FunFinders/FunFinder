f0691bfca0af5dc55d72101ba92c6a13991ce7c3
Kevin Mach <kevinmach.cs@gmail.com>
2026-02-17 22:26:18 -0800

    Add some database query building functions

--- src/database.py
+++ src/database.py
@@ -1,4 +1,6 @@
-from .data import OpeningHours, UserModel
+from .data import OpeningHours, Place
+from math import isclose
+import typing
 
 def get_place(database, place_id):
     cursor = database.cursor()
@@ -7,7 +9,7 @@ def get_place(database, place_id):
     """
     params = {"place_id": place_id}
     place_tuple = cursor.execute(QUERY, params).fetchone()
-    return UserModel(place_tuple)
+    return Place(place_tuple)
 
 def get_hours(database, place_id):
     cursor = database.cursor()
@@ -34,6 +36,38 @@ def filter_price(min=0, max=4):
         else:
             clause = f"price <= {max}"
     return clause
+
+def filter_type_disjunctive(types: list[str] | str):
+    if type(types) is str:
+        return f"instr(place_types, {types})"
+    return " OR ".join((f"instr(place_types, {tag})" for tag in types))
+
+def filter_type_conjunctive(types: list[str] | str):
+    if type(types) is str:
+        return f"instr(place_types, {types})"
+    return " AND ".join((f"instr(place_types, {tag})" for tag in types))
+
+def filter_rating(min: float=1.0, max: float=5.0):
+    if min < 1.0:
+        min = 1.0
+
+    if min > 5.0:
+        min = 5.0
+
+    if max > 5.0:
+        max = 5.0
+
+    if min > max:
+        max = min
+
+    QUERY = ""
+    if not isclose(min, 1.0):
+        QUERY = f"rating >= {min}"
+    if not isclose(max, 5.0):
+        if QUERY != "":
+            QUERY += " AND "
+        QUERY += f"rating <= {max}"
+    return QUERY
 
 def filter_():
     pass
