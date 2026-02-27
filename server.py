58102f369b2fa6140fa942852cec82c68b5f3e41
Kevin Mach <kevinmach.cs@gmail.com>
2026-02-27 13:41:47 -0800

    Create Flask server file with skeleton methods

---
+++ server.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, request, make_response
+# use jsonify
+# make_response ... .set_cookie(key, value)?
+# request.cookies?
+
+app = Flask(__name__)
+
+def get_user():
+    pass
+
+@app.route("/...")
+def get_places():
+    pass
+
+@app.route("/...")
+def get_place():
+    pass
+
+@app.route("/...")
+def store_user():
+    pass
+
+@app.route("/...")
+def create_user():
+    pass
+
+# @app.route("/...")
+# def add_saved
+
+# @app.route("/...")
+# def add_liked
+
+# @app.route("/...")
+# def add_visited
+
+# @app.route("/...")
+# def add_preferred_type
+
+# @app.route("/...")
+# def remove_saved
+
+# @app.route("/...")
+# def remove_liked
+
+# @app.route("/...")
+# def remove_visited
+
+# @app.route("/...")
+# def remove_preferred_type
+
+# @app.route("/...")
+# def get_recommendation
+
+# @app.route("/...")
+# def search
+
+# @app.route("/...")
+# def 
