75f06122e6b21d9b2896396d213169f724bd0f7f
Jason Phan <jsonphan@gmail.com>
2026-03-03 16:02:27 -0800

    Created flask server to give us places in json format. Modifed index.tsx to display jsonified data from the server.

--- server.py
+++ server.py
@@ -1,28 +1,73 @@
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify
+from flask_cors import CORS  # Add this import
+from src.data import UserModel, Place
+from src.recommendation import rank_places
+from src.user import get_user
+import sqlite3
+
 # use jsonify
 # make_response ... .set_cookie(key, value)?
 # request.cookies?
 
 app = Flask(__name__)
+CORS(app)
+@app.route("/places", methods=['GET'])
 
-def get_user():
-    pass
-
-@app.route("/...")
 def get_places():
-    pass
+    # connect to database
+    conn = sqlite3.connect("places.db")
+    cursor = conn.cursor()
 
-@app.route("/...")
-def get_place():
-    pass
+    # list of places
+    req = cursor.execute("select * from places")
+    places = [Place(x) for x in req]
 
-@app.route("/...")
-def store_user():
-    pass
+    # get user preferences
+    user = UserModel()
+    user = get_user("user.json", user)
 
-@app.route("/...")
-def create_user():
-    pass
+    # rank it baby
+    ranking = rank_places(places, user)
+    ranking.sort(key=lambda x: x[0], reverse=True)
+    
+    # going to return top 20 places, but can change later
+    places_json = []
+
+    # only top 20 for now, can change later
+    for weight, place in ranking[:20]:
+        places_json.append({
+            "id":place.id,
+            "name":place.display_name,
+            "rating":place.rating,
+            "priceLevel":place.price
+        })
+    
+    conn.close()
+    return jsonify(places_json)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+
+
+# def get_user():
+#     pass
+
+# @app.route("/...")
+# def get_places():
+#     pass
+
+# @app.route("/...")
+# def get_place():
+#     pass
+
+# @app.route("/...")
+# def store_user():
+#     pass
+
+# @app.route("/...")
+# def create_user():
+#     pass
 
 # @app.route("/...")
 # def add_saved
@@ -56,3 +101,5 @@ def create_user():
 
 # @app.route("/...")
 # def 
+
+
